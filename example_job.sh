ee2308bba62b3c9283372a8f87e98a80e6b5dc1f
Cankun Wang <flykun0620@gmail.com>
2020-09-21 04:15:02 -0400

    Change directory

--- example_job.sh
+++ example_job.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #PBS -l walltime=01:00:00
-#PBS -l nodes=1:ppn=16:gpus=1
-#PBS -l mem=24GB
+#PBS -l nodes=1:ppn=2:gpus=1
+#PBS -l mem=8GB
 #PBS -m n
 #PBS -j oe
 #PBS -S /usr/bin/bash
